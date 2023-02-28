12ce3a8060d322be55b27340d48ea5772c669e1d
Great Joycs <pavigreatjoycs@gmail.com>
2023-02-28 15:16:00 +0530

    Added to src/main/java

---
+++ src/main/java/com/luv2code/pkg/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+  public int employeeId;
+  public String employeeName;
+  
+  public int getEmployeeId(){
+    return this.employeeId;
+  }
+  
+  public String getEmployeeName(){
+    return this.employeeName;
+  }
+  
+}

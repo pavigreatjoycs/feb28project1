7b8dcb0594ff7726b5c7bf15d7fd4ae4522ebe00
Great Joycs <76424765+pavigreatjoycs@users.noreply.github.com>
2023-02-28 15:09:28 +0530

    Update Employee.java

--- src/com/luv2code/Employee.java
+++ src/com/luv2code/Employee.java
@@ -2,4 +2,12 @@ public class Employee {
   public int employeeId;
   public String employeeName;
   
+  public int getEmployeeId(){
+    return this.employeeId;
+  }
+  
+  public String getEmployeeName(){
+    return this.employeeName;
+  }
+  
 }
